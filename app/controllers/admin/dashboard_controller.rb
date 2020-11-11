c9b3c85a4880396252fde87ce01801b6586a9e2f
Etienne LC <etlecoent@gmail.com>
2020-11-11 20:41:51 +0000

    Implemented the dashboard view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV["ADMIN_PASSWORD"]
   
   def show
+    @products_nb = Product.all.count
+    @categories_nb = Category.all.count
   end
 end
