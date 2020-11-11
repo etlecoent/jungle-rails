819913750eaf90a5291210d2f770bf1c761d7bca
Etienne LC <etlecoent@gmail.com>
2020-11-11 22:02:59 +0000

    Added the about route, controller and the show view

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  resource :about, only: [:show] do
+    root to: 'about#show'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
