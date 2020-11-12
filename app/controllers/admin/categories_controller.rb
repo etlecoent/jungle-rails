9f1ab9b6364d05a912e276a01f40a77d10a03765
Etienne LC <etlecoent@gmail.com>
2020-11-12 15:54:27 +0000

    Changed the redirecting on form submission

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -13,7 +13,7 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :category], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
