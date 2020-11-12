0211ae2c9c9c9041002d2fce96f0f0d45a50d32e
Etienne LC <etlecoent@gmail.com>
2020-11-12 17:20:20 +0000

    Added the session logic on registeration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to 'root'
+    else
+      redirect to 'new'
+    end 
   end
 
   private
