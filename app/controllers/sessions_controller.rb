6b3e4c3df1362e36ab283119c206f4ebe90038b1
Etienne LC <etlecoent@gmail.com>
2020-11-12 17:51:49 +0000

    Added logic to authenticate the user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to 'root'
+    else 
+      redirect_to 'user/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to 'user/login'
   end
 
 end
