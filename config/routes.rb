3a32e682b18d18de653be497847d76771c5f0f54
Etienne LC <etlecoent@gmail.com>
2020-11-12 16:25:20 +0000

    Created the user ressources with new and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Rails.application.routes.draw do
     root to: 'about#show'
   end
 
+  resources :users, only: [:new, :create]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
