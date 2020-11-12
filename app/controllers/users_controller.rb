ba1fdd8a091b02229217b322ef8ad570ec7e73b4
Etienne LC <etlecoent@gmail.com>
2020-11-12 20:30:39 +0000

    Changed the rerouting because of the route refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,15 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to 'root'
+      redirect_to '/'
     else
-      redirect to 'new'
+      redirect_to '/register'
     end 
   end
 
