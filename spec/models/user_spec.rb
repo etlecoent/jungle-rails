dad82c6fa4b560994313c0ccd3ca9bd54d8c497a
Etienne LC <etlecoent@gmail.com>
2020-11-16 20:31:47 +0000

    Created the pseudocode for the validations specifications

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  describe 'Validations' do
+    #password section
+      #password & password_confirmation matchs
+      #required when creating the model
+
+    #email section
+      #each one is unique & not case sensitive
+
+    #email, first name, last name are required
+  end
+end
