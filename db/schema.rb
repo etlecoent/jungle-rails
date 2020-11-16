52dd0ba9e91aa5b95d53a0538470df4f2ea0e17f
Etienne LC <etlecoent@gmail.com>
2020-11-16 18:46:43 +0000

    Added rspec to the dev environment

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,15 @@ ActiveRecord::Schema.define(version: 20201112161036) do
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
+  create_table "ursers", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
