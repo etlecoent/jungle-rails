e554cad9ef0f0a64d3750e7190f50a9fc42cfbe7
Etienne LC <etlecoent@gmail.com>
2020-11-17 18:51:46 +0000

    Code cleaning

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
-   # SETUP
-   before :each do
-    @category = Category.create! name: 'Apparel'
+  # SETUP
+  before :each do
+  @category = Category.create! name: 'Apparel'
 
     10.times do |n|
-      @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
-        description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
-        quantity: 10,
-        price: 64.99
-      )
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
     end
   end
 
