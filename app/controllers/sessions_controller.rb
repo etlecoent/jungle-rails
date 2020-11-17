f2c3bfb12475b3de5899146cc66325f08a38b709
Etienne LC <etlecoent@gmail.com>
2020-11-17 01:41:33 +0000

    Code cleaning

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
     if user = User.authenticate_with_credentials(params[:email], params[:password])
-      session[:user_id] = @user.id
+      session[:user_id] = user.id
       redirect_to '/'
     else 
       redirect_to '/login'
